836ae6f417e161b7fbef660f60ed09044ba5cdcd
LuanPetruitis <luanpetruitis@hotmail.com>
2022-04-11 20:43:58 -0300

    Página Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,28 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 
-import HomeHeader from "../../components/Header/HomeHeader";
+// import HomeHeader from "../../components/Header/HomeHeader";
+import NuvemFormas from "../../assets/nuvem-formas.svg";
+import Logo from "../../assets/logo.png";
+import HomeOffice from "../../assets/home_office.png";
+
 
 export default function Home() {
   return (
     <>
-      <HomeHeader />
+      <div className="h-screen w-screen">
+        <img src={NuvemFormas} className="w-screen h-1/4" />
+        <div className="h-1/2 flex p-11">
+          <div className="">
+            <img src={Logo} />
+            <p>Uma plataforma aberta para que você possa discutir os assuntos do seu interesse. Cadastre-se e conheça seus recursos.</p>
+          </div>
+          <div className="overflow-hidden">
+            <img src={HomeOffice} className="h-full"  />
+          </div>
+        </div>
+        <img src={NuvemFormas} className="w-screen h-1/4" />
+      </div>
     </>
   );
 }
