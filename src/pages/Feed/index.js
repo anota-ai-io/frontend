f943a09c7bca772b8056b3d57cd0c0634d3ce1cc
DanielNery <danielpontesnery@gmail.com>
2022-05-08 17:25:37 -0300

    feat: Iniciando chat e ajuste na versão mobile.

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -13,6 +13,7 @@ import User from '../../assets/user.svg'
 import Image from '../../assets/image.svg'
 import Smile from '../../assets/smile.svg'
 import Vector from '../../assets/Vector.svg'
+import Menu from '../../assets/menu.svg'
 
 
 export default function Feed() {
@@ -20,7 +21,6 @@ export default function Feed() {
     <>
       <div className="h-screen w-screen grid grid-cols-1 md:grid-cols-7">
         
-        {/* Bloco esquerdo */}
         <div className="hidden md:block col-span-2 border-t-4">
           <img src={Logo} className="pl-6 mt-2" />
 
@@ -63,26 +63,24 @@ export default function Feed() {
 
         </div>
 
-        {/* Bloco do meio */}
-        <div className="col-span-3 border-t-4 border-2 grid">
+        <div className="col-span-1 md:col-span-3 border-t-4 border-2 grid">
           
           <div className="md:hidden row-span-1 border-b-2 p-5 mt-5">
-            Menu Hamburguer
+            <img src={Menu} ></img>
           </div>
 
-          {/* Pesquisa e foto */}
-          <div className="row-span-1 border-b-2 p-5 mt-5">
+          <div className="row-span-1 max-h-52	 border-b-2 p-5 mt-5">
             
-            <div className=" flex items-center justify-center">
-              <div className="border-2 rounded-full ml-10 mr-5"><img src={User1} className="p-6"></img></div>
-              <input type="text" placeholder="O que está pensando?" className="mr-10 w-9/12 h-16 focus:outline-none"></input>
+            <div className=" flex flex-col md:flex-row items-center justify-center">
+              <div className="border-2 rounded-full md:ml-10 md:mr-5"><img src={User1} className="p-6"></img></div>
+              <input type="text" placeholder="O que está pensando?" className="md:mr-10 md:w-9/12 h-16 focus:outline-none"></input>
             </div>
             
-            <div className="flex justify-center">
-              <hr className=" w-7/12"></hr>
+            <div className="hidden md:flex md:justify-center">
+              <hr className="hidden md:block w-7/12"></hr>
             </div>
 
-            <div className="flex flex-row mt-2 ml-32">
+            <div className="flex flex-row justify-center md:justify-start mt-2 md:ml-32">
               <img className="mr-5" src={Image}></img>
               <img className="mr-5" src={Smile}></img>
               <img className="mr-5" src={Vector}></img>
@@ -91,22 +89,14 @@ export default function Feed() {
           </div>
 
           <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
-          <div className="row-span-1"></div>
+          
 
         </div>
 
 
 
-        {/* Bloco direito */}
         <div className="hidden md:grid col-span-2 border-t-4 gap-5">
 
-          {/* Pesquisa */}
           <div className="row-auto flex justify-center items-center mt-2">
             <input type="text" className="input border-2 rounded-full w-11/12 h-16"></input>
           </div>
@@ -115,25 +105,44 @@ export default function Feed() {
             <div className="border-2 rounded-3xl w-11/12 h-64"></div>
           </div>
 
-          <div className="row-auto flex justify-center items-start">
-            <div className="border-2 rounded-3xl w-11/12 h-64"></div>
-          </div>
-          
-          {/* <div className="row-auto justify-center items-start">
-            <div className="border-2 rounded-full flex flex-row justify-center items-center w-9/12 overflow-hidden">
-              
-              <div className="flex flex-row">
-                <h1>#Teste</h1>
-              </div>
+          <div className="row-auto flex justify-center items-start text-center">
+            
+            <div className="border-2 rounded-3xl w-11/12 h-64 overflow-scroll">
 
-              <div className="flex flex-row">
-                <h1>#Teste</h1>
-              </div>
+              <span><h2 className="mt-2">CHAT</h2></span>
 
+              <span className="border-2 rounded-full flex p-2 m-2 items-center">
+                <div className="flex-none border-2 rounded-full"><img src={User1} className="p-4"></img></div>
+                <span className="flex-1 ml-2">Fulano</span>
+                <div className="flex-1 w-10/12 flex justify-end"><div className="rounded-full h-3 w-3 bg-green-500"></div></div>
+              </span>
+
+              <span className="border-2 rounded-full flex p-2 m-2 items-center">
+                <div className="flex-none border-2 rounded-full"><img src={User1} className="p-4"></img></div>
+                <span className="flex-1 ml-2">Fulano</span>
+                <div className="flex-1 w-10/12 flex justify-end"><div className="rounded-full h-3 w-3 bg-green-500"></div></div>
+              </span>
+
+              <span className="border-2 rounded-full flex p-2 m-2 items-center">
+                <div className="flex-none border-2 rounded-full"><img src={User1} className="p-4"></img></div>
+                <span className="flex-1 ml-2">Fulano</span>
+                <div className="flex-1 w-10/12 flex justify-end"><div className="rounded-full h-3 w-3 bg-green-500"></div></div>
+              </span>
+
+              <span className="border-2 rounded-full flex p-2 m-2 items-center">
+                <div className="flex-none border-2 rounded-full"><img src={User1} className="p-4"></img></div>
+                <span className="flex-1 ml-2">Fulano</span>
+                <div className="flex-1 w-10/12 flex justify-end"><div className="rounded-full h-3 w-3 bg-green-500"></div></div>
+              </span>
+
+              <span className="border-2 rounded-full flex p-2 m-2 items-center">
+                <div className="flex-none border-2 rounded-full"><img src={User1} className="p-4"></img></div>
+                <span className="flex-1 ml-2">Fulano</span>
+                <div className="flex-1 w-10/12 flex justify-end"><div className="rounded-full h-3 w-3 bg-green-500"></div></div>
+              </span>
 
             </div>
-          </div> */}
-          
+          </div>
           
         </div>
 
