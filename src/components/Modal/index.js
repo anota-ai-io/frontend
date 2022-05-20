73d7dca5125937ccccac41bd9a2072417228fae8
DanielNery <danielpontesnery@gmail.com>
2022-05-20 14:05:28 -0300

    feat: Efeito pulse skeleton e ajustes mobile.

---
+++ src/components/Modal/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+import Image from '../../assets/image.svg'
+import Smile from '../../assets/smile.svg'
+import Vector from '../../assets/Vector.svg'
+
+import { useForm } from "react-hook-form";
+
+export default function Modal() {
+  const [showModal, setShowModal] = React.useState(false);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
+  return (
+    <>
+      <button
+        className="bg-blue-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 animacao-padrao"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+
+        Publicação
+      </button>
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none animacao-padrao"
+          >
+            <div className="relative w-6/12 h-3/6 ">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Nova Publicação
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                    
+                    <form onSubmit={handleSubmit(onSubmit)} className="justify-center text-center">
+                        <textarea placeholder="  Escreva sua públicação..." {...register("content", {})} className="w-full h-32 md:h-64"/>
+                    </form >
+
+                    <div className="flex flex-row justify-center md:justify-start mt-5">
+                        <img className="mr-5" src={Image}></img>
+                        <img className="mr-5" src={Smile}></img>
+                        <img className="mr-5" src={Vector}></img>
+                    </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    className="bg-blue-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="submit"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Públicar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, } from "react";
 
 // import ComponentSkeleton from '../../components/Skeleton/index.js';
 import { parseCookies } from 'nookies';
+import Modal from "../../components/Modal";
 
 import Logo from '../../assets/logo.png'
 import User1 from '../../assets/user1.svg'
@@ -12,9 +13,7 @@ import Home from '../../assets/home.svg'
 import Mail from '../../assets/mail.svg'
 import PlusSquare from '../../assets/plus-square.svg'
 
-import Image from '../../assets/image.svg'
-import Smile from '../../assets/smile.svg'
-import Vector from '../../assets/Vector.svg'
+
 import Menu from '../../assets/menu.svg'
 
 import Comments from '../../assets/message-circle.svg'
@@ -29,8 +28,10 @@ import { Link } from "react-router-dom";
 export default function Feed() {  
   const [posts, setPosts] = useState([])
   const [menuMobileState, setMenuMobileState] = useState(false)
+  const [loadPostsState, setLoadPotsState] = useState(true)
 
   useEffect(() => {
+    console.log(loadPostsState)
     const cookies = parseCookies();
     console.log(cookies['anotaai.token'])
     fetch("https://anotaifsp.herokuapp.com/api/feed", {
@@ -44,13 +45,16 @@ export default function Feed() {
       .then(response => {
         setPosts(response.response.posts)
       })
+      .then(response => {
+        setLoadPotsState(false)
+        console.log(loadPostsState)
+      })
       .catch(err => {
         console.log(err)
       })
   }, [])
 
   function setMenuMobile() {
-    return
     setMenuMobileState(!menuMobileState)
   }
 
@@ -60,52 +64,90 @@ export default function Feed() {
   }
 
 
+  function renderPublicationsAnimated() {
+    return (
+      <div className="row-span-1 border grid grid-cols-12 animate-pulse">
+        <div className="ml-2 md:ml-0  col-span-2 row-span-6">
+          <div className="flex items-center justify-center mt-5"> <div className="border-2 rounded-full bg-slate-200"> <img src={User1} className="p-3 md:p-6 "></img> </div> </div>
+        </div>
+
+        <div className=" col-span-10 row-span-2 flex justify-self-start md:mt-5 md:mr-5">
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-base md:text-xl mt-2 w-32 h-6 bg-slate-200"></span></div>
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-sm md:text-lg mt-2 w-32 h-6 bg-slate-200"></span></div>
+          <div className="hidden md:flex items-center justify-start mt-5 ml-2"><span className="text-xs md:text-lg mt-2 w-32 h-6 bg-slate-200"></span></div>
+        </div>
+
+
+        <div className="col-span-9 row-span-1 ">
+          <div className="flex md:hidden items-center justify-start ml-2"><span className="text-xs md:text-lg mt-2 w-32 h-6 bg-slate-200"></span></div>
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-sm whitespace-pre-line break-all"></span></div>
+        </div>
+
+
+        <div className="col-span-10 row-span-1 ">
+          <div className="flex items-center justify-center mt-5 ml-2">
+            <div className="h-96 w-full mr-8 bg-slate-200"></div>  
+          </div>
+        </div>
+
+
+        <div className="col-span-8 row-span-1 mb-5">
+          <div className="flex flex-row items-center justify-between mt-5">
+            <span className="flex flex-row"><img src={Comments} className="mr-2" />  </span>
+            <span className="flex flex-row"><img src={Hearts} className="mr-2" />  </span>
+            <span className="flex flex-row"><img src={Share2} className="mr-2" />  </span>
+            <span className="flex flex-row"><img src={Downloads} className="mr-2" />  </span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   function renderPublications() {
     return posts.map(post => (
-        <div className="row-span-1 border grid grid-cols-12">
 
-          <div className="col-span-2 row-span-6">
-            <div className="flex items-center justify-center mt-5"> <div className="border-2 rounded-full "> <img src={User1} className="p-6"></img> </div> </div>
-          </div>
-
-          {/* PRIMEIRA LINHA */}
-          <div className="col-span-10 row-span-2 flex justify-self-start mt-5 mr-5">
-            <div className="flex items-center justify-start mt-5 ml-2"><span className="text-xl"> { post.user.name } </span></div>
-            <div className="flex items-center justify-start mt-5 ml-2"><span className="text-lg"> @{ post.user.username } </span></div>
-            <div className="flex items-center justify-start mt-5 ml-2"><span className="text-lg">{ formatDate(post.createdAt) }</span></div>
-          </div>
-
-          {/* SEGUNDA LINHA */}
-
-          <div className="col-span-9 row-span-1 ">
-            <div className="flex items-center justify-start mt-5"><span className="text-sm whitespace-pre-line break-all">{ post.content }</span></div>
-          </div>
-
-          {/* TERCEIRA LINHA */}
-
-          <div className="col-span-10 row-span-1 ">
-            <div className="flex items-center justify-center mt-5"><img className="mt-2 w-full mr-8" src={post.images[0] ? post.images[0] : ''} /></div>
-          </div>
-
-          {/* QUINTA LINHA */}
-
-          <div className="col-span-8 row-span-1 mb-5">
-            <div className="flex flex-row items-center justify-between mt-5">
-              <span className="flex flex-row"><img src={Comments} className="mr-2" /> {post.commentsCounter} </span>
-              <span className="flex flex-row"><img src={Hearts} className="mr-2" /> {post.likesCounter} </span>
-              <span className="flex flex-row"><img src={Share2} className="mr-2" /> {post.sharesCounter} </span>
-              <span className="flex flex-row"><img src={Downloads} className="mr-2" /> {post.downloadsCounter} </span>
-            </div>
-          </div>
-
+      <div className="row-span-1 border grid grid-cols-12 ">
+        <div className="ml-2 md:ml-0  col-span-2 row-span-6">
+          <div className="flex items-center justify-center mt-5"> <div className="border-2 rounded-full "> <img src={User1} className="p-3 md:p-6"></img> </div> </div>
         </div>
+
+        <div className=" col-span-10 row-span-2 flex justify-self-start md:mt-5 md:mr-5">
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-base md:text-xl"> { post.user.name } </span></div>
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-sm md:text-lg"> @{ post.user.username } </span></div>
+          <div className="hidden md:flex items-center justify-start mt-5 ml-2"><span className="text-xs md:text-lg">{ formatDate(post.createdAt) }</span></div>
+        </div>
+
+
+        <div className="col-span-9 row-span-1 ">
+          <div className="flex md:hidden items-center justify-start ml-2"><span className="text-xs md:text-lg">{ formatDate(post.createdAt) }</span></div>
+          <div className="flex items-center justify-start mt-5 ml-2"><span className="text-sm whitespace-pre-line break-all">{ post.content }</span></div>
+        </div>
+
+
+        <div className="col-span-10 row-span-1 ">
+          <div className={ loadPostsState ? "animate-pulse flex space-x-4" : "flex items-center justify-center mt-5 ml-2" }>
+            { loadPostsState ? <div className="h-96 w-full mr-8 bg-slate-200"></div> : <img className="mt-2 w-full mr-8" src={post.images[0]} /> }
+          </div>
+        </div>
+
+
+        <div className="col-span-8 row-span-1 mb-5">
+          <div className="flex flex-row items-center justify-between mt-5">
+            <span className="flex flex-row"><img src={Comments} className="mr-2" /> {post.commentsCounter} </span>
+            <span className="flex flex-row"><img src={Hearts} className="mr-2" /> {post.likesCounter} </span>
+            <span className="flex flex-row"><img src={Share2} className="mr-2" /> {post.sharesCounter} </span>
+            <span className="flex flex-row"><img src={Downloads} className="mr-2" /> {post.downloadsCounter} </span>
+          </div>
+        </div>
+      </div>
+        
       ))
   }
 
 
   return (
     <>
-      <div className="h-screen w-screen grid grid-cols-1 md:grid-cols-7 overflow-hidden">
+      <div className="h-screen w-screen grid grid-cols-1 md:grid-cols-12 overflow-hidden">
         
 
         {/* ESQUERDA - MENU E USUÁRIO */}
@@ -113,7 +155,7 @@ export default function Feed() {
 
           <div className="flex flex-row justify-between md:row-span-1 md:block md:p-5">
             <img src={Logo} className="" />
-            <img src={xClose} className={menuMobileState ? "block mr-5" : "hidden " } width={25} height={25} onClick={setMenuMobile()}/>
+            <img src={xClose} className={menuMobileState ? "block mr-5" : "hidden " } width={25} height={25} onClick={setMenuMobile}/>
           </div>
 
           <div className="row-span-6">
@@ -172,7 +214,9 @@ export default function Feed() {
             </nav>
           </div>
 
-          <div className="row-span-1 flex flex-row">
+          
+
+          <div className="row-span-1 flex flex-row justify-between">
               <Link
                     to="/perfil"
                     className="flex flex-col items-start align-bottom p-6"
@@ -182,66 +226,41 @@ export default function Feed() {
               </Link>
           </div>
 
+          <div className="p-6">
+            <Modal />
+          </div>
+
         </div>
 
 
         {/* CENTRO - PUBLICAÇÕES E POSTS */}
         <div  
-          className={menuMobileState ? "hidden" : "col-span-1 md:col-span-3 border-t-4 border-2 h-screen overflow-y-scroll animacao-padrao"}>
+          className={menuMobileState ? "hidden" : "col-span-1 md:col-span-6 border-t-4 border-2 h-screen overflow-y-scroll animacao-padrao"}>
+          
           
 
           {/* MENU MOBILE */}
-          <div className="md:hidden row-span-1 border-b-2 p-5 mt-5 " onClick={e => this.setMenuMobile()}>
+          <div className="md:hidden row-span-1 border-b-2 p-5 mt-5 " onClick={setMenuMobile}>
             <img src={Menu} className=""></img>
           </div>
-
-          {/* ADICIONAR PUBLICACAO */}
-          <div className="row-span-1 grid grid-cols-1 md:grid-cols-12 gap-2 border-b-2 p-5 justify-center align-middle text-center ">
-            
-
-            <div className="col-span-1 md:col-span-2 row-span-1 justify-center align-middle text-center mb-5 md:mb-0  ">
-              <div className="flex justify-center align-middle text-center"><div className="border-2 rounded-full"><img src={User1} className="p-6"></img></div></div>
-            </div>
-
-            <div className="col-span-1 md:col-span-10 row-span-1 justify-center align-middle text-center mt-5 mb-5">
-              <div className="flex justify-center align-middle text-center "><textarea placeholder="O que está pensando?" className="w-96 h-32 md:h-16 focus:outline-none"></textarea></div>
-            </div>
-
-            <div className="col-span-1 md:col-span-10 flex flex-row justify-center md:justify-start mt-5 md:ml-32">
-              <img className="mr-5" src={Image}></img>
-              <img className="mr-5" src={Smile}></img>
-              <img className="mr-5" src={Vector}></img>
-            </div>
-
-
-            {/* <div className=" flex flex-col md:flex-row items-center justify-center">
-              <div className="border-2 rounded-full md:ml-10 md:mr-5"><img src={User1} className="p-6"></img></div>
-              <textarea placeholder="O que está pensando?" className="md:mr-10 md:w-9/12 h-16 focus:outline-none"></textarea>
-            </div>
-
-            <div className="flex flex-row justify-center md:justify-start mt-5 md:ml-32">
-              <img className="mr-5" src={Image}></img>
-              <img className="mr-5" src={Smile}></img>
-              <img className="mr-5" src={Vector}></img>
-            </div> */}
-
-          </div>
-
-          
+    
           <div className="row-span-1 border flex text-center justify-center">
-            <a href="#" className="text-blue-600 font-bold">Visualizar mais publicações</a>
+            <Link to="/feed" className="text-blue-600 font-bold">Visualizar mais publicações</Link>
           </div>
 
 
           {/* LISTA DE PUBLICACOES */}
-          {renderPublications()}
+          {
+            loadPostsState ? renderPublicationsAnimated() : renderPublications()
+          }
+    
           
 
         </div>
 
 
         {/* DIREITA -  PESQUISA DE ASSUNTOS E EM ALTA */}
-        <div className="hidden md:grid col-span-2 border-t-4 gap-5">
+        <div className="hidden md:grid col-span-4 border-t-4 gap-5">
 
           <div className="row-auto flex justify-center items-center mt-2">
             <input type="text" placeholder="      Pesquisar no Anota Aí" className="input border-2 rounded-full w-11/12 h-16"></input>
