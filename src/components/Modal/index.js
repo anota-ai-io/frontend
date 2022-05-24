5bc12ba4175778d21df9731d58d89d63dd104125
Rhenan <diasrhenan@gmail.com>
2022-05-24 10:11:24 -0300

    Adicionando envio de imagens no formulário de criação de post

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,55 +1,56 @@
 import React, { useState } from "react";
 
-import Image from '../../assets/image.svg'
-import Smile from '../../assets/smile.svg'
-import Vector from '../../assets/Vector.svg'
+import Image from "../../assets/image.svg";
+import Smile from "../../assets/smile.svg";
+import Vector from "../../assets/Vector.svg";
 
 import { useForm } from "react-hook-form";
-import { parseCookies } from 'nookies';
-
+import { parseCookies } from "nookies";
 
 export default function Modal() {
   const [showModal, setShowModal] = React.useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [images, setImages] = useState([]);
 
-  const handleSubmitPosts = async data => {
+  const handleSubmitPosts = async (data) => {
     const cookies = parseCookies();
-    console.log(images)
     const formData = new FormData();
-    formData.append('content', data.content);
-    formData.append('hashtags', 'John123');
-    formData.append('hashtags', 'John1234');
-    // formData.append('images', ['John123']);
+    formData.append("content", data.content);
+    formData.append("hashtags", JSON.stringify(["hastagh1", "hasshtag2"]));
+    images.forEach((img) => formData.append("images", img));
 
     await fetch("https://anotaifsp.herokuapp.com/api/post", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${cookies['anotaai.token']}`
+        Authorization: `Bearer ${cookies["anotaai.token"]}`,
       },
-      body: formData
+      body: formData,
     })
-        .then(response => response.json())
-        .then(data => {
-          setShowModal(false)
-        })
-        .catch(err => {
-          console.log(err.message)
-        })
+      .then((response) => response.json())
+      .then((data) => {
+        setShowModal(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   };
 
   const hiddenFileInput = React.useRef(null);
 
   function chooseImage(event) {
-    hiddenFileInput.current.click()
+    hiddenFileInput.current.click();
   }
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     const formData = new FormData();
     const filesXmlArray = Array.from(event.target.files);
-    filesXmlArray.forEach(file => formData.append('files', file));
-    console.log(filesXmlArray)
-    setImages(filesXmlArray)
+    filesXmlArray.forEach((file) => formData.append("files", file));
+    console.log(filesXmlArray);
+    setImages(filesXmlArray);
   };
 
   return (
@@ -59,22 +60,17 @@ export default function Modal() {
         type="button"
         onClick={() => setShowModal(true)}
       >
-
         Publicação
       </button>
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none animacao-padrao"
-          >
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none animacao-padrao">
             <div className="relative w-full h-full md:w-6/12 md:h-3/6 ">
               {/*content*/}
               <div className="border-0 p-4 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Nova Publicação
-                  </h3>
+                  <h3 className="text-3xl font-semibold">Nova Publicação</h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -85,16 +81,29 @@ export default function Modal() {
                   </button>
                 </div>
                 {/*body*/}
-                <form onSubmit={handleSubmit(handleSubmitPosts)} className="justify-center text-center">
+                <form
+                  onSubmit={handleSubmit(handleSubmitPosts)}
+                  className="justify-center text-center"
+                >
                   <div className="relative flex-auto">
-                      
-                    <textarea placeholder="  Escreva sua públicação..." {...register("content", {})} className="w-full h-96 m-0 md:h-64"/>
+                    <textarea
+                      placeholder="  Escreva sua públicação..."
+                      {...register("content", {})}
+                      className="w-full h-96 m-0 md:h-64"
+                    />
 
                     <div className="flex flex-row justify-center md:justify-start mt-5">
-                        <img className="mr-5" src={Image} onClick={chooseImage} />
-                        <input type="file" ref={hiddenFileInput} onChange={handleChange} id="file-upload" className="hidden" multiple />
-                        <img className="mr-5" src={Smile} />
-                        <img className="mr-5" src={Vector} />
+                      <img className="mr-5" src={Image} onClick={chooseImage} />
+                      <input
+                        type="file"
+                        ref={hiddenFileInput}
+                        onChange={handleChange}
+                        id="file-upload"
+                        className="hidden"
+                        multiple
+                      />
+                      <img className="mr-5" src={Smile} />
+                      <img className="mr-5" src={Vector} />
                     </div>
                   </div>
                   {/*footer*/}
