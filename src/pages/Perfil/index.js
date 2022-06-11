9a78507f86226b1e0e35dba1f91aae1b5e940a26
LuanPetruitis <luanpetruitis@hotmail.com>
2022-06-10 22:08:03 -0300

    Ajustando o carregamento automático dos posts no perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,44 +1,11 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { LogoutIcon, ArrowLeftIcon } from '@heroicons/react/outline';
 import { Link, useParams } from 'react-router-dom';
 import { parseCookies } from 'nookies';
-import Modal from '../../components/Modal'
+import Modal from '../../components/Modal';
 import { useForm } from 'react-hook-form';
 import Image from '../../assets/image.svg';
-
-const postsTest = [
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-    {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-        img: 'http://localhost:8080',
-    },
-];
 
 export default function Perfil() {
     const { username } = useParams();
@@ -60,23 +27,23 @@ export default function Perfil() {
     const handleSubmitUser = async data => {
         const cookies = parseCookies();
         const formData = new FormData();
-        if ("" !== data.bio) {
+        if ('' !== data.bio) {
             formData.append('bio', data.bio);
         }
 
         if (image) {
             formData.append('profilePicture', image);
         }
-        
+
         // console.log(JSON.stringify(formData))
-        console.log(formData)
-        
+        console.log(formData);
+
         fetch('https://anotaifsp.herokuapp.com/api/user', {
             method: 'PATCH',
             headers: {
                 Authorization: `Bearer ${cookies['anotaai.token']}`,
             },
-            body: formData
+            body: formData,
         })
             .then(response => response.json())
             .then(data => {
@@ -116,10 +83,8 @@ export default function Perfil() {
         )
             .then(response => response.json())
             .then(response => {
-                console.log(response.response.user);
                 setUser(response.response.user);
-                // setPosts(response.response.posts);
-                setPosts(postsTest);
+                setPosts(response.response.posts);
             })
             .finally(() => {
                 setLoaded(true);
@@ -133,7 +98,6 @@ export default function Perfil() {
         <>
             {loaded && (
                 <div className="h-screen w-screen overflow-x-hidden">
-                    
                     <div className="w-screen h-1/4 flex justify-between items-center p-4">
                         <div>
                             <Link to="/feed">
@@ -166,69 +130,69 @@ export default function Perfil() {
                         </button>
                     </div>
                     {showModal ? (
-                            <Modal closeModal={closeModal}>
-                                <form
-                                    onSubmit={handleSubmit(handleSubmitUser)}
-                                    className="justify-center text-center"
-                                >
-                                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                        <h3 className="text-3xl font-semibold">
-                                            Editar Perfil do Usuário
-                                        </h3>
-                                        <button
-                                            className="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                            onClick={closeModal}
-                                        >
-                                            <span className="text-black opacity-5 h-6 w-6 text-4xl block outline-none focus:outline-none">
-                                                X
-                                            </span>
-                                        </button>
-                                    </div>
-                                    <div className="relative flex-auto">
-                                        <textarea
-                                            placeholder="  Escreva sua bio..."
-                                            {...register('bio', {})}
-                                            className="w-full h-40 m-0 md:h-40"
-                                        />
+                        <Modal closeModal={closeModal}>
+                            <form
+                                onSubmit={handleSubmit(handleSubmitUser)}
+                                className="justify-center text-center"
+                            >
+                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 className="text-3xl font-semibold">
+                                        Editar Perfil do Usuário
+                                    </h3>
+                                    <button
+                                        className="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                        onClick={closeModal}
+                                    >
+                                        <span className="text-black opacity-5 h-6 w-6 text-4xl block outline-none focus:outline-none">
+                                            X
+                                        </span>
+                                    </button>
+                                </div>
+                                <div className="relative flex-auto">
+                                    <textarea
+                                        placeholder="  Escreva sua bio..."
+                                        {...register('bio', {})}
+                                        className="w-full h-40 m-0 md:h-40"
+                                    />
 
-                                        <div className="flex flex-row justify-center md:justify-start mt-5">
-                                            <img
-                                                className="mr-5"
-                                                src={Image}
-                                                onClick={chooseImage}
-                                            />
-                                            <input
-                                                type="file"
-                                                ref={hiddenFileInput}
-                                                onChange={handleChange}
-                                                id="file-upload"
-                                                className="hidden"
-                                            />
-                                            <span>
-                                                {image
-                                                    ? 1 + ' imagem'
-                                                    : 'Adicionar imagem'}{' '}
-                                            </span>
-                                        </div>
+                                    <div className="flex flex-row justify-center md:justify-start mt-5">
+                                        <img
+                                            className="mr-5"
+                                            src={Image}
+                                            onClick={chooseImage}
+                                        />
+                                        <input
+                                            type="file"
+                                            ref={hiddenFileInput}
+                                            onChange={handleChange}
+                                            id="file-upload"
+                                            className="hidden"
+                                        />
+                                        <span>
+                                            {image
+                                                ? 1 + ' imagem'
+                                                : 'Adicionar imagem'}{' '}
+                                        </span>
                                     </div>
-                                    {/*footer*/}
-                                    <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                        <button
-                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                            type="button"
-                                            onClick={closeModal}
-                                        >
-                                            Cancelar
-                                        </button>
-                                        <button
-                                            className="bg-blue-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                            type="submit"
-                                        >
-                                            Publicar
-                                        </button>
-                                    </div>
-                                </form>
-                            </Modal>
+                                </div>
+                                {/*footer*/}
+                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                    <button
+                                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="button"
+                                        onClick={closeModal}
+                                    >
+                                        Cancelar
+                                    </button>
+                                    <button
+                                        className="bg-blue-700 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        type="submit"
+                                    >
+                                        Publicar
+                                    </button>
+                                </div>
+                            </form>
+                        </Modal>
                     ) : null}
                     <div className="w-screen grid gap-4 grid-cols-1 md:grid-cols-3 p-6">
                         {posts.map(post => (
